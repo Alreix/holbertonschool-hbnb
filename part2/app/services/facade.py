3d29c530a53f2741cb91fbea282a13338d663dfb
frxjoan <12224@holbertonstudents.com>
2026-02-18 10:50:58 +0100

    Set up HBnB project structure with app factory, in-memory repository, and facade

---
+++ part2/app/services/facade.py
@@ -0,0 +1,16 @@
+from app.repository import InMemoryRepository
+
+class HBnBFacade:
+    def __init__(self):
+        self.user_repo = InMemoryRepository()
+        self.place_repo = InMemoryRepository()
+        self.review_repo = InMemoryRepository()
+        self.amenity_repo = InMemoryRepository()
+
+    # Placeholder method for creating a user
+    def create_user(self, user_data):
+        pass
+
+    # Placeholder method for fetching a place by ID
+    def get_place(self, place_id):
+        pass
