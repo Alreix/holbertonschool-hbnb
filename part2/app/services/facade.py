a47e94de8667b2983bb3b047d1ed07eb5f3fb4e2
Alreix <alreixdev@gmail.com>
2026-02-21 17:30:47 +0100

    feat(facade.py): implementing logical for user treatment

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,16 +1,17 @@
 from app.repository import InMemoryRepository
+from app.models.user import User
 
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
-        self.amenity_repo = InMemoryRepository()
 
-    # Placeholder method for creating a user
     def create_user(self, user_data):
-        pass
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
 
-    # Placeholder method for fetching a place by ID
-    def get_place(self, place_id):
-        pass
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
